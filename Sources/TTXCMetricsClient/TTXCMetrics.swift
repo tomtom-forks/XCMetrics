3f386e5e7fa5dd4f4a09b2df92adc6269fadc800
DiegoDiaz-TomTom <diego.diaz@tomtom.com>
2024-12-09 11:13:10 +0100

    feat: create plugin to collect github actions metadata

--- Sources/TTXCMetricsClient/TTXCMetrics.swift
+++ Sources/TTXCMetricsClient/TTXCMetrics.swift
@@ -3,18 +3,19 @@ import XCMetricsClient
 import XCMetricsPlugins
 
 public struct TTXCMetrics {
-  public static func main() {
-    let metrics = XCMetrics.parseOrExit()
-    let configuration = XCMetricsConfiguration()
-    configuration.add(plugin: ThermalThrottlingPlugin().create())
-    configuration.add(plugin: LoadAveragePlugin().create())
-
-    // The git directory is usually Xcode's `$SRCROOT` environment variable
-    let gitDirectory: String? = ProcessInfo.processInfo.environment["SRCROOT"]
-    if let gitDirectory {
-        configuration.add(plugin: GitPlugin(gitDirectoryPath: gitDirectory).create())
+    public static func main() {
+        let metrics = XCMetrics.parseOrExit()
+        let configuration = XCMetricsConfiguration()
+        configuration.add(plugin: ThermalThrottlingPlugin().create())
+        configuration.add(plugin: LoadAveragePlugin().create())
+        configuration.add(plugin: GitHubActionsPlugin().create())
+        
+        // The git directory is usually Xcode's `$SRCROOT` environment variable
+        let gitDirectory: String? = ProcessInfo.processInfo.environment["SRCROOT"]
+        if let gitDirectory {
+            configuration.add(plugin: GitPlugin(gitDirectoryPath: gitDirectory).create())
+        }
+        
+        metrics.run(with: configuration)
     }
-
-    metrics.run(with: configuration)
-  }
 }
---
+++ Sources/XCMetricsPlugins/GitHubActions.swift
@@ -0,0 +1,30 @@
+import Foundation
+import XCMetricsClient
+import XCMetricsUtils
+
+public struct GitHubActionsPlugin {
+    
+    private let shell: ShellOutFunction
+    
+    public init(shell: @escaping ShellOutFunction = shellGetStdout) {
+        self.shell = shell
+    }
+    
+    public func create() -> XCMetricsPlugin {
+        return XCMetricsPlugin(name: "Load Average", body: { _ -> [String : String] in
+            guard !getEnv("CI").isEmpty else { return [:] }
+            
+            return [
+                "github_job": getEnv("GITHUB_JOB"),
+                "github_repository": getEnv("GITHUB_REPOSITORY"),
+                "github_run_attempt": getEnv("GITHUB_RUN_ATTEMPT"),
+                "github_run_id": getEnv("GITHUB_RUN_ID"),
+                "github_workflow": getEnv("GITHUB_WORKFLOW"),
+            ]
+        })
+    }
+    
+    private func getEnv(_ key: String) -> String {
+        return ProcessInfo.processInfo.environment[key] ?? ""
+    }
+}
